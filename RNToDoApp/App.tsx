1cf8179735be8d8742845a4b4832ff0957f91897
Tony <gth1123@naver.com>
2022-09-12 15:08:50 +0900

    3.4 ~ 3.5 AsyncStorage

--- RNToDoApp/App.tsx
+++ RNToDoApp/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -8,8 +8,10 @@ import {
   TextInput,
   NativeSyntheticEvent,
   TextInputSubmitEditingEventData,
+  ScrollView,
 } from "react-native";
 import { theme } from "./color";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type Todo = {
   [id: string]: {
@@ -17,6 +19,8 @@ type Todo = {
     work: boolean;
   };
 };
+
+const STORAGE_KEY = "@toDos";
 
 export default function App() {
   const [isWorking, setIsWorking] = useState(true);
@@ -36,9 +40,30 @@ export default function App() {
     };
     // const newToDos = Object.assign({}, toDos, newToDo);
     // setToDos(newToDos);
-    setToDos({ ...toDos, ...newToDo });
+    const newToDos = { ...toDos, ...newToDo };
+    setToDos(newToDos);
+    return newToDos;
   };
-  const handleSubmitText = (
+  const loadToDos = async () => {
+    try {
+      const toDos = await AsyncStorage.getItem(STORAGE_KEY);
+      console.log("toDos", toDos);
+      if (toDos !== null) {
+        setToDos(JSON.parse(toDos));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const saveToDos = async (value: any) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem(STORAGE_KEY, jsonValue);
+    } catch (e) {
+      // saving error
+    }
+  };
+  const handleSubmitText = async (
     e: NativeSyntheticEvent<TextInputSubmitEditingEventData>
   ) => {
     // console.log(e.nativeEvent.text);
@@ -47,10 +72,15 @@ export default function App() {
       return;
     }
 
-    addToDo(text);
+    const newToDos = addToDo(text);
     setText("");
+    await saveToDos(newToDos);
   };
   console.log(toDos);
+
+  useEffect(() => {
+    loadToDos();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -86,6 +116,16 @@ export default function App() {
           style={styles.input}
           placeholder={isWorking ? "Add a To do" : "Where do you want to go?"}
         />
+        <ScrollView style={styles.toDoContainer}>
+          {toDos &&
+            Object.entries(toDos).map(([id, toDo]) =>
+              toDo.work === isWorking ? (
+                <View key={id} style={styles.toDo}>
+                  <Text style={styles.toDoText}>{toDo.text}</Text>
+                </View>
+              ) : null
+            )}
+        </ScrollView>
       </View>
     </View>
   );
@@ -113,5 +153,20 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     marginTop: 20,
     fontSize: 18,
+  },
+  toDoContainer: {
+    marginTop: 20,
+  },
+  toDo: {
+    backgroundColor: theme.gray,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+  },
+  toDoText: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: "white",
   },
 });
--- docs/3_to-do-list.md
+++ docs/3_to-do-list.md
@@ -56,4 +56,35 @@
 
 - Object hash map을 사용함
   - array를 사용할 줄 알았는데 이렇게 하면 list를 렌더링할 때 어떻게 하는지 지켜봐야겠다
+    - Object.keys 를 사용
+    - 나는 Object.entries를 사용
   - 그리고 제거할 땐 filter가 아닌 다른 방법으로 할 것 같은데 그것도 궁금하다
+
+## 3.4 Paint To do list
+
+## 3.5 Persist
+
+```jsx
+// Work과 travel의 구분
+{
+  Object.entries(toDos).map(([id, toDo]) =>
+              toDo.work === isWorking ? (
+                <View key={id} style={styles.toDo}>
+                  <Text style={styles.toDoText}>{toDo.text}</Text>
+                </View>
+              ) : null
+}
+```
+
+### AsyncStorage
+
+- To do list를 핸드폰에 저장하기 위해 사용
+  - 브라우저의 localStorage와 거의 비슷
+- https://docs.expo.dev/versions/latest/sdk/async-storage/
+  - https://react-native-async-storage.github.io/async-storage/docs/usage/
+- expo install @react-native-async-storage/async-storage
+
+  - expo install은 npm install과 같지만 현재 사용중인 expo 버전과 같은 버전의 모듈을 설치해줌
+
+- setItem : string(json 데이터)를 key와 함께 저장
+- getItem : 저장했던 string(json 데이터)를 key로 불러옴
